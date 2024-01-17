1fc7afdf066afaafbe633f860452696fbd59ccb8
Djalma Hideki Yamamoto <69691272+YH1d3k12@users.noreply.github.com>
2024-01-17 15:15:15 -0300

    Feat: created entities

---
+++ game.js
@@ -0,0 +1,55 @@
+const rocks = [...document.querySelectorAll(".rock")];
+let currentFriendlyCreature;
+let currentHostileCreature;
+
+window.onload = function () {
+	startGame();
+}
+
+function startGame() {
+	setInterval(createFriendlyCreature, 1000);
+	setInterval(createHostileCreature, 1500);
+}
+
+function getRandomTile() {
+	let tile = Math.floor(Math.random() * rocks.length);
+	return tile;
+}
+
+function createFriendlyCreature() {
+	if (currentFriendlyCreature) {
+		currentFriendlyCreature.innerHTML = "";
+	}
+
+	let rock = getRandomTile();
+
+	if (currentHostileCreature && currentHostileCreature.getAttribute("tile") == rock) {
+		return;
+	}
+
+	currentFriendlyCreature = rocks[rock];
+	
+	let creature = document.createElement("img");
+	creature.src = "./images/monty-mole.png";
+
+	currentFriendlyCreature.appendChild(creature);
+}
+
+function createHostileCreature() {
+	if (currentHostileCreature) {
+		currentHostileCreature.innerHTML = "";
+	}
+
+	let rock = getRandomTile();
+
+	if (currentFriendlyCreature && currentFriendlyCreature.getAttribute("tile") == rock) {
+		return;
+	}
+
+	currentHostileCreature = rocks[rock];
+
+	let creature = document.createElement("img");
+	creature.src = "./images/piranha-plant.png";
+
+	currentHostileCreature.appendChild(creature);
+}
